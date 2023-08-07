3715f41045f25050a3876e7d98af47fa9453fc3f
Akash Singh <furiousakash111@gmail.com>
2023-08-08 00:50:21 +0530

    Nodemailer added for otp

--- models/Otp.js
+++ models/Otp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mailSender = require('../utils/mailSender');
 
 const otpSchema = new mongoose.Schema({
     email: {
@@ -16,5 +17,20 @@ const otpSchema = new mongoose.Schema({
         expires: 5*60,
     }
 });
+
+async function sendVerificationEmail(email, otp){
+    try {
+        const mailResponse = await mailSender(email, "Verification mail from StudyNation", otp);
+        console.log("Mail response sent successfully: ", mailResponse);
+    } catch (error) {
+        console.log("Error occured while sending mail !!");
+        console.error(error);
+    }
+}
+
+otpSchema.pre('save', async function(next){
+    await sendVerificationEmail(this.email, this.otp);
+    next();
+})
 
 module.exports = mongoose.model("OTP", otpSchema);
---
+++ utils/mailSender.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const mailSender = async (email, title, body) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            }
+        })
+        const info = await transporter.sendMail({
+            from: "StudyNation || Akash Singh",
+            to: `${email}`,
+            subject: `${title}`,
+            html: `${body}`,
+        })
+        console.log(info);
+        return info;
+    } catch (error) {
+        console.log("Could not send the mail !!");
+        console.error(error);
+    }
+}
+
+module.exports = mailSender;
